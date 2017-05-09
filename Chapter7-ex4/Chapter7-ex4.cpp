5bc651b3a44ed08b066322774e6329fdb389b21b
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-09 22:50:41 +0100

    Add project files.

---
+++ Chapter7-ex4/Chapter7-ex4.cpp
@@ -0,0 +1,48 @@
+// Chapter7-ex4.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+void maxInt(int* store_, int decision_);
+
+int main()
+{
+	int store[100];
+
+	int decision = 0;
+	std::cout << "How many numbers do you want to type? max 100: ";
+	std::cin >> decision;
+	for (int i = 0; i < decision; i++)
+	{
+		std::cout << "Please type nr " << i + 1 << ": ";
+		std::cin >> store[i];
+	}
+
+	maxInt(store, decision);
+	
+	system("pause");
+    return 0;
+}
+void maxInt(int* store_, int decision_)
+{
+		int tmpIndex = 0;
+		int tmpValue = store_[0];
+
+	for (int i = 0; i < decision_ -  1; i++)
+	{
+
+		for (int j = 0; j < decision_; j++)
+		{
+			if (store_[tmpIndex] <= store_[j+1])
+			{
+				tmpIndex = j+1;
+				tmpValue = store_[j+1];
+				
+			}
+		}
+	}
+			std::cout << "Index number:" << tmpIndex << std::endl;
+			std::cout << "Largest number: " << tmpValue << std::endl;
+
+
+}
